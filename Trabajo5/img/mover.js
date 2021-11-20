a1914fc7d4451ca21a13c2059029221c6daa8242
d0sald0 <94731165+d0sald0@users.noreply.github.com>
2021-11-19 22:10:32 -0700

    Add files via upload

---
+++ Trabajo5/img/mover.js
@@ -0,0 +1,9 @@
+function mover_pag_1() {
+    location.href = "https://docs.google.com/forms/d/e/1FAIpQLSfKes20jYID74LA2wKXcy9X5kzlMhirlcqyGiA4hrAZM1-XAA/viewform?usp=sf_link";
+}
+function mover_pag_2() {
+    location.href = "https://classic.minecraft.net/";
+}
+function mover_pag_3() {
+    location.href = "https://fonts.google.com/specimen/Fruktur#standard-styles";
+}
